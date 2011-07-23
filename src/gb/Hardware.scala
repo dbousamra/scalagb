551616f5e329b660e9e9e39adf4cacdb8ec898fe
Dominic Bou-Samra <domlebo70@gmail.com>
2011-07-23 10:09:45 +1000

    Redid registers. Not sure what the best way to simulate bytes is. Having issues with Byte type in Scala not being able to use bitwise operators

--- src/gb/Hardware.scala
+++ src/gb/Hardware.scala
@@ -3,30 +3,25 @@ import scala.collection.mutable.BitSet
 
 class Registers {
   
-  var a, b, c, d, e, h, l, f : BitSet = BitSet()
+  var a, b, c, d, e, h, l, f : Int = 0
   
   var instructionPointer : BitSet = BitSet()
   var stackPointer : BitSet = BitSet()
   
   var mClock, tClock : Int = 0
   
-  val ZERO_FLAG : BitSet = BitSet(0x80)	
-  val SUBTRACT_FLAG : BitSet = BitSet(0x40)
-  val HALF_CARRY_FLAG : BitSet = BitSet(0x20)
-  val CARRY_FLAG : BitSet = BitSet(0x10)
-
-  
-  
-  def setFlag(flag: BitSet) = flag match {
-    case ZERO_FLAG => f |= ZERO_FLAG
-    case SUBTRACT_FLAG => f |= SUBTRACT_FLAG
-    case HALF_CARRY_FLAG => f |= HALF_CARRY_FLAG
-    case CARRY_FLAG => f |= CARRY_FLAG
+  def getFlag(flag : Int) = flag match {
+    case 0x80 if (f & 0x80) != 0 => true
+    case 0x40 if (f & 0x40) != 0 => true
+    case 0x20 if (f & 0x20) != 0 => true
+    case 0x10 if (f & 0x10) != 0 => true
   }
-  
 
- 
-  
-  
+  def setFlag2(flag : Int) = flag match {
+    case 0x80 => f |= 0x80
+    case 0x40 => f |= 0x40
+    case 0x20 => f |= 0x20
+    case 0x10 => f |= 0x10
+  }
   
 }
--- src/gb/Main.scala
+++ src/gb/Main.scala
@@ -5,7 +5,7 @@ object Main {
   def main(args: Array[String]): Unit = { 
     
     val x : Registers = new Registers()
-    x.setFlag(x.HALF_CARRY_FLAG)
+    x.setFlag2(0x30);
     println(x.f)
   }
 
