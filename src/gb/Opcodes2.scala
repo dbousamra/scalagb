8ad4ef6a729b4a1ad5dca97a347e37b4fc9592fd
co4222 <charles.ofarrell@mincom.com>
2011-08-02 08:49:20 +1000

    Present two options for op handling
    The trait example is overkill if that's the only way we use it.
    But pattern matching with the same cases more than 2-3 times can
    be done better with classes.

--- src/gb/Opcodes2.scala
+++ src/gb/Opcodes2.scala
@@ -100,10 +100,10 @@ class Opcodes2(cpu : Cpu) {
 	case 0xEA => LD_n_A16Write(cpu.a)
 	case 0xF2 => LD_A_C(cpu.a, cpu.c)
 	
-	case 0x3A => LDD_A_HLRead(cpu.a, cpu.h, cpu.l, "-")
-	case 0x32 => LDD_HL_A(cpu.h, cpu.l, cpu.a,"-")
-	case 0x2A => LDD_A_HLRead(cpu.a, cpu.h, cpu.l, "+")
-	case 0x22 => LDD_HL_A(cpu.h, cpu.l, cpu.a, "+")
+	case 0x3A => LDD_A_HLRead(cpu.a, cpu.h, cpu.l, -1)
+	case 0x32 => LDD_HL_A(cpu.h, cpu.l, cpu.a, |-|)
+	case 0x2A => LDD_A_HLRead(cpu.a, cpu.h, cpu.l, +1)
+	case 0x22 => LDD_HL_A(cpu.h, cpu.l, cpu.a, |+|)
 	
 	case 0xE0 => LDH_n_A(cpu.pc, cpu.a)
 	case 0xF0 => LDH_A_n(cpu.a, cpu.pc)
@@ -117,6 +117,10 @@ class Opcodes2(cpu : Cpu) {
 	
 			
   }
+
+  trait Op { val offset: Int }
+  case object |-| extends Op { override val offset = -1 }
+  case object |+| extends Op { override val offset = 1 }
   
   def LD_nn_n(register : Register) = {
     register.value = cpu.memory.readByte8(cpu.pc.value)
@@ -144,22 +148,15 @@ class Opcodes2(cpu : Cpu) {
       cpu.memory.writeByte8(cpu.memory.readByte16(cpu.pc.value), valueRegister.value)
   }
   
-  def LDD_A_HLRead(toRegister : Register, fromRegister : Register, fromRegister2 : Register, op : String) = {
+  def LDD_A_HLRead(toRegister : Register, fromRegister : Register, fromRegister2 : Register, op : Int) = {
     toRegister.value = cpu.memory.readByte8((fromRegister.value << 8) + fromRegister2.value)
-    op match {
-      case "+" => fromRegister2.value = (fromRegister2.value + 1) & 255
-      case "-" => fromRegister2.value = (fromRegister2.value + 1) & 255
-    }
+    fromRegister2.value = (fromRegister2.value + op) & 255
     if (fromRegister2.value == 255) fromRegister.value = (fromRegister.value - 1) & 255
   }
   
-  def LDD_HL_A(toRegister : Register, toRegister2 : Register, fromRegister : Register, op : String) = {
+  def LDD_HL_A(toRegister : Register, toRegister2 : Register, fromRegister : Register, op : Op) = {
     cpu.memory.writeByte8((toRegister.value << 8) + toRegister2.value, fromRegister.value)
-    op match {
-      case "+" => toRegister2.value = (toRegister2.value + 1) & 255
-      case "-" => toRegister2.value = (toRegister2.value - 1) & 255
-      
-    }
+    toRegister2.value = (toRegister2.value + op.offset) & 255
     if (toRegister2 == 255) toRegister.value = (toRegister.value - 1) & 255
   }
   
