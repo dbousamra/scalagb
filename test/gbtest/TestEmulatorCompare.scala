f0b5d0f334e50a5023687ac84ae87f8a210f0139
Dominic Bou-Samra <domlebo70@Dominics-MacBook-Air.local>
2011-08-06 10:20:14 +1000

    Changed the way the test runs. Now asserts on each line, rather then the whole collection

--- test/gbtest/TestEmulatorCompare.scala
+++ test/gbtest/TestEmulatorCompare.scala
@@ -3,36 +3,41 @@ package gbtest
 import org.scalatest.junit.AssertionsForJUnit
 import scala.collection.mutable.ListBuffer
 import org.junit._
+import org.junit.runner.JUnitCore
+import org.junit.runner.Result
 import Assert._
 import gb._
 
 class EmulatorCompare extends AssertionsForJUnit {
-  
+
   @Test def testEmulator() {
-    
-    var comparisonDepth = 15
+
+    var comparisonDepth = 50
     val romName = "roms/testRom1.gb"
     val stackTraceFileName = "test/gbtest/testRom1"
     val iterations = 100
     var x = 0
     val cpu: Cpu = new Cpu(romFilename = romName, DEBUG_MODE = true)
     cpu.reset()
-        while (x < iterations) {
-          cpu.run()
-          x += 1
-        }
-    
-    val stackTrace = readStackTrace(stackTraceFileName)
+    while (x < iterations) {
+      cpu.run()
+      x += 1
+    }
 
-    assertEquals(stackTrace.reverse.take(comparisonDepth), cpu.stackTrace.reverse.take(comparisonDepth))
+    val stackTrace = readStackTrace(stackTraceFileName)
+		
+	val range = 0.until(stackTrace.reverse.take(comparisonDepth).length)
+	for (i <- range) { 
+	  assertEquals(stackTrace.reverse.take(comparisonDepth)(i), cpu.stackTrace.reverse.take(comparisonDepth)(i)) 
+	}  
   }
-  
-  def readStackTrace(fileName : String) : List[String] = {
+
+  def readStackTrace(fileName: String): List[String] = {
     var stackTrace = List[String]()
-    for {(line) <- io.Source.fromFile(fileName).getLines} 
-		stackTrace = line :: stackTrace
-	return stackTrace
+    for { (line) <- io.Source.fromFile(fileName).getLines }
+      stackTrace = line :: stackTrace
+    return stackTrace
   }
-  
 }
+
   
