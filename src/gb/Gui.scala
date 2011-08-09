3dc5bc737d5a40dcf32cc6930f72ff14e18bec63
Dominic Bou-Samra <domlebo70@Dominics-MacBook-Air.local>
2011-08-09 11:51:48 +1000

    Added the timer event function to handle emulation speed

--- src/gb/Gui.scala
+++ src/gb/Gui.scala
@@ -2,45 +2,59 @@ package gb
 
 import swing.{ Panel, MainFrame, SimpleSwingApplication }
 import java.awt.{ Color, Graphics2D, Dimension }
+import java.awt.event._
+import java.awt
+import javax.swing.Timer
 
-class DataPanel(data: Array[Array[Color]]) extends Panel {
+object ColorPanel extends SimpleSwingApplication {
+  private var c: Color = new Color(0)
+  private val cpu: Cpu = new Cpu(romFilename = "roms/testRom1.gb", DEBUG_MODE = true)
+  cpu.reset()
+  private val frameTitle = "ScalaGB"
 
-  override def paintComponent(g: Graphics2D) {
-    val cpu: Cpu = new Cpu(romFilename = "roms/testRom1.gb", DEBUG_MODE = true)
-    //cpu.execute.
-    val dx = g.getClipBounds.width.toFloat / data.length
-    val dy = g.getClipBounds.height.toFloat / data.map(_.length).max
-    for {
-      x <- 0 until data.length	
-      y <- 0 until data(x).length
-      x1 = (x * dx).toInt
-      y1 = (y * dy).toInt
-      x2 = ((x + 1) * dx).toInt
-      y2 = ((y + 1) * dy).toInt
-    } {
-      data(x)(y) match {
-        case c: Color => g.setColor(c)
-        case _ => g.setColor(Color.WHITE)
-      }
-      g.fillRect(x1, y1, x2 - x1, y2 - y1)
-    }
-  }
-}
-
-object Draw extends SimpleSwingApplication {
-
-  val data = Array.ofDim[Color](25, 25)
-  data(0)(0) = Color.BLACK
-  data(4)(4) = Color.RED
-  data(0)(4) = Color.GREEN
-  data(4)(0) = Color.BLUE
+  var width: Int = 160
+  var height: Int = 144
 
   def top = new MainFrame {
-    
-    contents = new DataPanel(data) {
-      preferredSize = new Dimension(300, 300)
+    title = frameTitle
+    contents = p
+  }
+
+  val p = new Panel with ActionListener {
+    override val preferredSize = new Dimension(300, 300)
+
+    val data = Array.ofDim[Color](25, 25)
+    data(0)(0) = Color.BLACK
+    data(4)(4) = Color.RED
+    data(0)(4) = Color.GREEN
+    data(4)(0) = Color.BLUE
+
+    override def paintComponent(g: Graphics2D) {
+      val dx = g.getClipBounds.width.toFloat / data.length
+      val dy = g.getClipBounds.height.toFloat / data.map(_.length).max
+      for {
+        x <- 0 until data.length
+        y <- 0 until data(x).length
+        x1 = (x * dx).toInt
+        y1 = (y * dy).toInt
+        x2 = ((x + 1) * dx).toInt
+        y2 = ((y + 1) * dy).toInt
+      } {
+        data(x)(y) match {
+          case c: Color => g.setColor(c)
+          case _ => g.setColor(Color.WHITE)
+        }
+        g.fillRect(x1, y1, x2 - x1, y2 - y1)
+      }
+    }
+
+    def actionPerformed(e: ActionEvent) {
+      c = new Color((c.getRGB() + 1000) % 16777216)
+      repaint
+      cpu.run
     }
   }
-  
-  
+
+  val timer = new Timer(1, p) //can control timing from here.
+  timer.start()
 }
--- src/gb/Main.scala
+++ src/gb/Main.scala
@@ -2,11 +2,7 @@ package gb
 
 object Main {
 
-  var width: Int = 160
-  var height: Int = 144
-
   def main(args: Array[String]): Unit = {
-    var x = Draw
   }
 
 }
